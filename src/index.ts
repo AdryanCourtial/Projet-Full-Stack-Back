f79cb65a5afd61b2320d5665c6dea503823b9c45
RichetinB <baptisterichetin@gmail.com>
2025-11-06 14:46:38 +0100

    :tada: init project

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`Hello World! Environment: ${process.env.NODE_ENV}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
